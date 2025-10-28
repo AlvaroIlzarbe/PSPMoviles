98ee2f26b98a0bb54e136aecda76a2633e88c400
AlvaroIlzarbe <aiv879@educa.madrid.org>
2025-10-28 18:07:27 +0100

    Rename ThymeleadConstants to ThymeleafConstants

--- src/main/java/Config/ThymeleadConstants.java
+++ src/main/java/Config/ThymeleadConstants.java
@@ -1,4 +1,10 @@
 package Config;
 
-public class ThymeleadConstants {
+public final class ThymeleafConstants {
+
+    private ThymeleafConstants() {}
+    public static final String TEAMPLATE_KEY_BUSQUEDA ="com.dam.TemplateEngine";
+    public static final String TEAMPLATE_MODE = "HTML";
+    public static final String CHARACTER_ENCODING = "UTF-8";
+    public static final String CONTENT_TYPE = "text/html;charset=UTF-8";
 }
