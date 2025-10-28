e0afdc8ab882b85c3e3f9f52c9ff99d96857ca4c
AlvaroIlzarbe <aiv879@educa.madrid.org>
2025-10-28 18:07:06 +0100

    Update Messageconstants.java

--- src/main/java/Config/Messageconstants.java
+++ src/main/java/Config/Messageconstants.java
@@ -1,4 +1,19 @@
 package Config;
 
 public class Messageconstants {
+
+    private Messageconstants() {}
+
+
+    public static final String MSG_NUMERO_MAYOR = "El número a adivinar es mayor";
+    public static final String MSG_NUMERO_MENOR = "El número a adivinar es menor";
+    public static final String MSG_NUMERO_INVALIDO = "Por favor, introduce un número válido";
+    public static final String MSG_GANASTE = "¡Has ganado! El número era ";
+    public static final String MSG_GAME_OVER = "¡Game Over! El número era ";
+
+
+    public static final String VAR_MENSAJE = "mensaje";
+    public static final String VAR_ERROR = "error";
+
+
 }
