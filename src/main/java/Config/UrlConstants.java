f6beb1e94ac443b70c9a8da40dd8cca9138ff6c4
AlvaroIlzarbe <aiv879@educa.madrid.org>
2025-10-28 18:08:21 +0100

    Refactor UrlConstants class to be final and private

--- src/main/java/Config/UrlConstants.java
+++ src/main/java/Config/UrlConstants.java
@@ -1,4 +1,23 @@
 package Config;
 
-public class UrlConstants {
+public final class UrlConstants {
+    private UrlConstants() {}
+
+        public static final String HOME_URL = "/home";
+        public static final String LOGIN_URL = "/login";
+        public static final String PANTALLA_UNO="/pantallaUno";
+        public static final String PANTALLA_DOS="/pantallaDos";
+
+
+
+        public static final String TEMPLATE_LOCALIZACION = " /WEB-INF/teamplates/";
+        public static final String TEMPLATE_TIPO_ARCHIVO = " .html";
+
+
+        public static final String TEAMPLATE_INICIO = "inicio";
+        public static final String TEAMPLATE_PANTALLA_JUEGO = "juego";
+        public static final String TEAMPLATE_PANTALLA_FINAL = "fin";
+        public static final String TEAMPLATE_PUNTUACION = "puntuacion";
+si algo esta en negro borrar que es una bomba
+
 }
